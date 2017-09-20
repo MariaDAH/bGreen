20c2fe3b24e728d8d4b4793c66af57ed0724184b
Maria de Andres <deandresherrero@gmail.com>
2017-09-20 15:39:57 +0200

    Adding custom js, changing custom css

---
+++ bower_components/vendor/bootstrap/js/custom.js
@@ -0,0 +1,30 @@
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var geocoder = new google.maps.Geocoder();
+        /*var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });*/
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
