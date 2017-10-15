b760d44c21f5efbec6e09a4083bdef9c0049dbdf
Maria de Andres <deandresherrero@gmail.com>
2017-10-15 14:00:37 +0100

    Add jquery to display panels

--- bower_components/app/js/custom.js
+++ bower_components/app/js/custom.js
@@ -1,3 +1,4 @@
+//Initialize google maps
 function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -10,6 +11,7 @@ function initMap() {
         });
       }
 
+//Search location by address in google maps
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
@@ -24,6 +26,27 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
+
+
+  $("#login").click(function(){
+       var loginp = document.getElementById('loginpnl');
+       var searchp = document.getElementById('searchpnl');
+       //if(loginp.display == none){
+          var section = document.getElementById('mySection');
+          section.remove();
+           $("#loginpnl").slideToggle('slow');
+        //}
+      })
+
+      $("#search").click(function(){
+       var loginp = document.getElementById('loginpnl');
+       var searchp = document.getElementById('searchpnl');
+        //if(searchp.display==none){
+          var section = document.getElementById('mySection');
+          section.remove();
+          $("#searchpnl").slideToggle('slow');
+        //}
+      })
 
 //Validate credit card number/iban
 
